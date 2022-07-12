0e6627d109de4b84b472a2c9c9bed8b5fdf12dcb
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-07-11 22:13:35 -0300

    Aula 01

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main(){
-    print("Hello World")
+    println("Bem vindo ao Bytebank")
+
+     //Var é variável e Val não
+    val titular: String = "Leticia"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.00
+
+    saldo = 100.00
+
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("Saldo: $saldo")
 }
